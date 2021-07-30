9b4843bcd7564981ff9380731c31d50b0aa250a8
leraviola <80530483+leraviola@users.noreply.github.com>
2021-07-30 13:42:16 +0300

    Сделана страница с объявлением

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// const swiper = new Swiper('.swiper-container', {
+//   direction: 'horizontal',
+//   loop: true, //бесконечная прокрутка
+// });
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
